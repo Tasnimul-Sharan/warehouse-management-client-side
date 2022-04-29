838c8782b9d72e6e7316176c09b2b64ac41fb9c6
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2022-04-29 07:51:31 +0600

    added header

---
+++ src/Pages/Home/ExtraSection/ExtraSection.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ExtraSection = () => {
+  return (
+    <div>
+      <h1>extra part</h1>
+    </div>
+  );
+};
+
+export default ExtraSection;
---
+++ src/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXXYbke4KCRI063eO5qD70v0EdPNIPItE",
+  authDomain: "warehouse-management-264df.firebaseapp.com",
+  projectId: "warehouse-management-264df",
+  storageBucket: "warehouse-management-264df.appspot.com",
+  messagingSenderId: "950727384698",
+  appId: "1:950727384698:web:0585a13feb7a177e19a5c3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth;
